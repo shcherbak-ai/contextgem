4ca1f29472d0551e0a988c1a637d7b2db40c556a
SergiiShcherbak <sergii@shcherbak.ai>
2025-04-30 21:01:26 +0200

    build: version 0.2.1

--- contextgem/__init__.py
+++ contextgem/__init__.py
@@ -20,7 +20,7 @@
 ContextGem - Effortless LLM extraction from documents
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Shcherbak AI AS"
 
 from contextgem.public import (
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sys
 project = "ContextGem"
 copyright = "2025, Shcherbak AI AS"
 author = "Sergii Shcherbak"
-release = "0.2.0"
+release = "0.2.1"
 
 
 # Add path to the package
